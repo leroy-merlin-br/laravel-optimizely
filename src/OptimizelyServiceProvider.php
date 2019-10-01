05918780f8b77f512e7197e59d8d1a3a3238a4b3
aleferreiranogueira <ale.ferreiranogueira@gmail.com>
2019-10-01 20:06:22 -0300

    Add skeleton ServiceProvider, I gotta sleep

---
+++ src/OptimizelyServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\ServiceProvider;
+use Optimizely\Optimizely;
+
+class OptimizelyServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+}
