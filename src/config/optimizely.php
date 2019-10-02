b6c8ac81843e34cd0e2e69d1af86588bb8d364a6
aleferreiranogueira <ale.ferreiranogueira@gmail.com>
2019-10-02 14:26:27 -0300

    Add disk config

--- src/config/optimizely.php
+++ src/config/optimizely.php
@@ -1,5 +1,6 @@
 <?php
 return [
-  'path' => env('OPTIMIZELY_DATAFILE_PATH'),
-  'webhook_secret' => env('OPTIMIZELY_WEBHOOK_SECRET'),
+    'disk' => env (OPTIMIZELY_DISK),
+    'path' => env('OPTIMIZELY_DATAFILE_PATH'),
+    'webhook_secret' => env('OPTIMIZELY_WEBHOOK_SECRET'),
 ];
