b26f011f524aef7edb9295adbc9d3b357416e368
aleferreiranogueira <ale.ferreiranogueira@gmail.com>
2019-10-02 11:09:00 -0300

    Add webhook route on controller

--- src/Http/Controllers/OptimizelyController.php
+++ src/Http/Controllers/OptimizelyController.php
@@ -1,25 +1,31 @@
 <?php
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class OptimizelyController extends BaseController
 {
-    public function webhook(Request $request)
+    public function webhook(Request $request): JsonResponse
     {
         $datafileUrl = $request->input('data')['cdn_url'];
 
         if (!$datafileUrl) {
-            response()->json(['Could not get datafile URL'], 401);
+            return response()->json(['Could not get datafile URL'], 400);
         }
 
         $datafileContents = file_get_contents($datafileUrl);
+
+        if (false === $datafileContents) {
+            return response()->json(['Could not get datafile contents'], 400);
+        }
+
         $filename = config('optimizely.path') . '/' . 'optimizely_datafile';
 
         if (!file_put_contents($filename, $datafileContents)) {
-            response()->json(['Could not save file :('], 500);
+            return response()->json(['Could not save file :('], 500);
         }
-        
-        response()->json([], 201);
+
+        return response()->json([], 201);
     }
 }
