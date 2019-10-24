9fc1a833a9e56a7868401a4fa0fc61f1c7b36f85
aleferreiranogueira <ale.ferreiranogueira@gmail.com>
2019-10-24 14:34:21 -0300

    Remove Storage and get file from Redis

--- tests/Unit/OptimizelyFactoryTest.php
+++ tests/Unit/OptimizelyFactoryTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Tests\Unit;
 
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Storage;
 use LeroyMerlin\Optimizely\OptimizelyFactory;
 use Optimizely\Optimizely;
@@ -11,20 +12,12 @@ class OptimizelyFactoryTest extends TestCase
     public function test_should_instanciate_optimizely_with_config()
     {
         //Set
-        Storage::fake();
-
+        config(['optimizely.key' => 'optimizely_datafile']);
         $factory = new OptimizelyFactory();
 
-        config(['optimizely.filepath' => __DIR__ . '/../fixtures/datafile']);
-        config(['optimizely.disk' => 'sideralspace']);
-
         // Expectations
-        Storage::shouldReceive('disk')
-            ->with('sideralspace')
-            ->andReturnSelf();
-
-        Storage::shouldReceive('get')
-            ->with(__DIR__ . '/../fixtures/datafile')
+        Redis::shouldReceive('get')
+            ->with('optimizely_datafile')
             ->andReturn(file_get_contents(__DIR__ . '/../fixtures/datafile'));
 
         // Actions
